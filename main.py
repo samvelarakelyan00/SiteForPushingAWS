1fc5c0d880969e222e29d8d8ea7ec6f98bd79d5b
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2024-11-30 14:52:53 +0400

    add friends page

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ app = FastAPI()
 def main():
     return FileResponse("index.html")
 
+
 @app.get("/home")
 def home():
     return FileResponse("home.html")
+
+
+@app.get("/friends")
+def friends():
+    return FileResponse("friends.html")
