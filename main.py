eece23bbb415dc825972900dbedc5f08a1941075
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2024-11-30 13:52:00 +0400

    site V1

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def main():
+    return FileResponse("index.html")
+
+@app.get("/home")
+def home():
+    return FileResponse("home.html")
